de4420c1740462a389651fd0c7a9a6177141e38e
Eric Boutueil <e.boutueil@gmail.com>
2018-04-26 16:50:36 +0200

    test chess 3 repos combination

--- app/app.js
+++ app/app.js
@@ -1,14 +1,33 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+angular.module('nywton.chessboard.app', ['nywton.chessboard'])
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+.config(['nywtonChessboardConfigProvider', function nywtonChessConfigConfig(chessboardProvider) {
+  chessboardProvider.pieceTheme('../bower_components/chessboard.js/dist/img/chesspieces/wikipedia/{piece}.png');
+  chessboardProvider.draggable(true).position('start');
+}])
+
+.controller('BodyCtrl', [function() {
+
+}])
+
+.directive('chessboardDebug', [function () {
+  var directive = {
+    restrict: 'A',
+    priority: 1,
+    scope : {
+      board: '=chessboardDebug',
+    },
+    template: '<div>' +
+     '<button type="button" data-ng-click="_debug = !_debug">show debug</button>' +
+     '<div class="game-debug-container" data-ng-show="_debug">' +
+       '<span class="label label-info">board {{board.name}}</span>' +
+       '<p><code>fen()? {{board.fen()}}</code></p>'+
+       '<p><code>position()? {{board.position() | json}}</code></p>'+
+       '<p><code>orientation()? {{board.orientation()}}</code></p>'+
+     '</div>' +
+   '</div>',
+  };
+
+  return directive;
 }]);
